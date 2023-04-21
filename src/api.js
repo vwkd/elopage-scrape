3814d60cc3f1b14f71920df43f41a292cb102f14
vwkd <33468089+vwkd@users.noreply.github.com>
2023-04-21 14:44:42 +0400

    scrape: refactor more into api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,48 @@
-export function makeRequest(url, token, user_agent) {
+import dotenv from "dotenv";
+dotenv.config();
+
+import { delay, random_number } from "./utils.js";
+
+const USER_AGENT = process.env.USER_AGENT;
+const DELAY = process.env.DELAY;
+const DELAY_OFFSET = process.env.DELAY_OFFSET;
+
+/**
+ * Get lessons from API
+ */
+export async function getLessons(course_session_id, token) {
+  // console.debug(`Fetching lessons from API ...`);
+
+  const lessonsUrl = `https://api.elopage.com/v1/payer/course_sessions/${course_session_id}/lessons?page=1&query=&per=10000&sort_key=id&sort_dir=desc&course_session_id=${course_session_id}`;
+
+  // delay +- random offset, random_number
+  await delay(random_number(DELAY, DELAY_OFFSET));
+
+  const lessonsResponse = await makeRequest(lessonsUrl, token);
+  const lessons = await lessonsResponse.json();
+  console.info(`Got ${lessons.data.total_count} lessons`);
+
+  return lessons;
+}
+
+/**
+ * Get content of lesson from API
+ */
+export async function getContent(lesson_id, content_page_id, course_session_id, token) {
+  // console.debug(`Fetching content '${content_page_id}' of lesson '${lesson_id}' from API ...`);
+
+  const contentUrl = `https://api.elopage.com/v1/payer/course_sessions/${course_session_id}/lessons/${lesson_id}/content_pages/${content_page_id}?screen_size=desktop`;
+
+  // delay +- random offset, random_number
+  await delay(random_number(DELAY, DELAY_OFFSET));
+
+  const contentResponse = await makeRequest(contentUrl, token);
+  const content = await contentResponse.json();
+
+  return content;
+}
+
+async function makeRequest(url, token) {
   return fetch(url, {
     "headers": {
       "accept": "application/json, text/plain, */*",
@@ -11,7 +55,7 @@ export function makeRequest(url, token, user_agent) {
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-site",
-      "user-agent": user_agent,
+      "user-agent": USER_AGENT,
     },
     // "referrer": "https://elopage.com/",
     // "referrerPolicy": "strict-origin-when-cross-origin",
--- src/scrape.js
+++ src/scrape.js
@@ -3,43 +3,24 @@ dotenv.config()
 
 import { writeFile } from "node:fs/promises";
 import { login } from "./login.js"
-import { delay, random_number } from "./utils.js";
 import { sortLessons } from "./sort.js";
-import { makeRequest } from "./api.js";
+import { getLessons, getContent } from "./api.js";
 
-const USER_AGENT = process.env.USER_AGENT;
 const START_URL = process.env.START_URL;
-
-const DELAY = process.env.DELAY;
-const DELAY_OFFSET = process.env.DELAY_OFFSET;
 
 const LESSONS_FILEPATH = "lessons.json";
 const CONTENT_FILEPATH = "content.json";
-
-const LESSONS_URL = (course_session_id) => `https://api.elopage.com/v1/payer/course_sessions/${course_session_id}/lessons?page=1&query=&per=10000&sort_key=id&sort_dir=desc&course_session_id=${course_session_id}`;
-
-const CONTENT_URL = (course_session_id, lesson_id, content_page_id) => `https://api.elopage.com/v1/payer/course_sessions/${course_session_id}/lessons/${lesson_id}/content_pages/${content_page_id}?screen_size=desktop`;
 
 console.info(`Start scraping course '${START_URL}' ...`);
 
 const { token, course_session_id } = login();
 
-// get lessons
-console.info(`Fetching lessons from API ...`);
-const lessonsUrl = LESSONS_URL(course_session_id);
-// delay +- random offset, random_number
-await delay(random_number(DELAY, DELAY_OFFSET));
-const lessonsResponse = await makeRequest(lessonsUrl, token, USER_AGENT);
-const lessons = await lessonsResponse.json();
-// await writeFile(LESSONS_FILEPATH, JSON.stringify(lessons));
-console.info(`Got ${lessons.data.total_count} lessons`);
-
-// sort lessons
+console.info(`Scraping lesson index ...`)
+const lessons = await getLessons(course_session_id, token);
 sortLessons(lessons);
 await writeFile(LESSONS_FILEPATH, JSON.stringify(lessons));
 
-// get content page for each lesson
-console.info(`Fetching content pages from API ...`)
+console.info(`Scraping lesson content ...`)
 const content = [];
 const lessonsArray = lessons.data.list;
 for (const lessonsObj of lessonsArray) {
@@ -55,17 +36,14 @@ for (const lessonsObj of lessonsArray) {
   }
 
   if (active === false) {
-    console.info(`Fetching inactive lesson '${lesson_id}' - '${content_page_id}' - '${title}' ...`);
+    console.info(`Scraping [INACTIVE] '${title}' ...`);
   } else {
-    console.info(`Fetching lesson '${lesson_id}' - '${content_page_id}' - '${title}' ...`);
+    console.info(`Scraping '${title}' ...`);
   }
 
-  const contentUrl = CONTENT_URL(course_session_id, lesson_id, content_page_id);
-  // delay +- random offset, random_number
-  await delay(random_number(DELAY, DELAY_OFFSET));
-  const contentResponse = await makeRequest(contentUrl, token, USER_AGENT);
-  const contentJson = await contentResponse.json();
-  content.push(contentJson);
+  const content_page = await getContent(lesson_id, content_page_id, course_session_id, token);
+
+  content.push(content_page);
 }
 
 await writeFile(CONTENT_FILEPATH, JSON.stringify(content));
